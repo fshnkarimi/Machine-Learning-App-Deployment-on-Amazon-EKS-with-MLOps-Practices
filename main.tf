a3f02484503434b303ffdb60130b5d357dbbb9cf
Afshin <afshinkarimi67@gmail.com>
2023-08-04 12:39:35 +0200

    terraform & CI/CD added

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.21"
+  subnets         = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+  vpc_id          = "vpc-abcde012"
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "m5.large"
+      key_name      = "my-key-name"
+    }
+  }
+}
